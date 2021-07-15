e8de2792422dbae0c2bd4eefe25ea919838c84c4
emmy-espinoza <emilceespinoza22@gmail.com>
2021-07-15 00:17:49 -0300

    midificaciones css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const elementosCarousel = document.querySelectorAll('.carousel');
+    const elementosCarousel = document.querySelectorAll('.carousel'); /*Inicializa el carousel*/
 M.Carousel.init(elementosCarousel,{
-    duration: 150, 
-    dist: -80, /*distancia entre imagenes*/
-    shift: 5,
-    padding: 5,
+    duration: 150, /*tiempo de transición de las imágenes*/
+    dist: -80, /*distancia de persperctiva, se superponen*/
+    shift: 5,/*distamcia entre cada una*/
+    padding: 5, /*cantidad del espacio entre el borde y el contenido del elemento*/
     numVisible: 5, /*las imagenes visibles*/
-    indicators: true,
-    noWrap: false,
+    indicators: true,/*Nos muestra los indicadores (los circulos de las imagenes)*/
+    noWrap: false, /*comienza desde */
 } );
 });
